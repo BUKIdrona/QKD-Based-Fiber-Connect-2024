57a7e9323802c7d3f2839f2dd33cb41c902bf2dc
BUKIdrona <deepak.mandal.ug21@nsut.ac.in>
2024-07-10 02:04:59 +0530

    updated hamburger

--- small_left.js
+++ small_left.js
@@ -1,6 +1,20 @@
-const toggleButton = document.getElementById('nav-toggle');
-    const navLinks = document.getElementById('navMenu');
 
-    toggleButton.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    })
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const navToggle = document.getElementById("nav-toggle");
+        const navMenu = document.getElementById("navMenu");
+    
+        navToggle.addEventListener("click", function() {
+            navMenu.classList.toggle("active");
+        });
+    
+        document.addEventListener("click", function(event) {
+            const isClickInside = navMenu.contains(event.target) || navToggle.contains(event.target);
+    
+            if (!isClickInside) {
+                navMenu.classList.remove("active");
+            }
+        });
+    });
+    
