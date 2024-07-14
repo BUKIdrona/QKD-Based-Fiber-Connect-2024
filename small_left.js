f2a51ee63d12eea6ea23a6653737c9ab4c460cfc
BUKIdrona <deepak.mandal.ug21@nsut.ac.in>
2024-07-14 15:49:53 +0530

    changed parton page

--- small_left.js
+++ small_left.js
@@ -1,6 +1,3 @@
-
-
-
     document.addEventListener("DOMContentLoaded", function() {
         const navToggle = document.getElementById("nav-toggle");
         const navMenu = document.getElementById("navMenu");
